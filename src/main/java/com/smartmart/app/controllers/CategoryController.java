a3bfb6bbfa7ca999f1472719a0c085ed835030c4
sonawaneshivam <shiv.sonawane09@gmail.com>
2025-04-18 22:37:47 +0530

    cahnrgs maade

--- src/main/java/com/smartmart/app/controllers/CategoryController.java
+++ src/main/java/com/smartmart/app/controllers/CategoryController.java
@@ -3,6 +3,7 @@ package com.smartmart.app.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.smartmart.app.models.Category;
 import com.smartmart.app.services.CategoryService;
 
-
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/category")
 public class CategoryController {
