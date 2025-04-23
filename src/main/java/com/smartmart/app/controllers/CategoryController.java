4f447da36bd771ce69d33b14b9f3bf468dffb66a
Ganesh Pawar <pawarganeshb123@gmail.com>
2025-04-23 12:43:16 +0530

    controller methods optimized

--- src/main/java/com/smartmart/app/controllers/CategoryController.java
+++ src/main/java/com/smartmart/app/controllers/CategoryController.java
@@ -15,16 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.smartmart.app.models.Category;
 import com.smartmart.app.services.CategoryService;
 
-
 @RestController
 @RequestMapping("/category")
 public class CategoryController {
 
-    
 	@Autowired
 	private CategoryService categoryService;
 
-   
 	@PostMapping("/add")
 	public String addCategory(@RequestBody Category category) {
 		if (categoryService.isAddNewCategory(category)) {
@@ -33,12 +30,12 @@ public class CategoryController {
 			return "category is not added";
 		}
 	}
-	
+
 	@GetMapping("/showAll")
-	public List<Category> showData(){
+	public List<Category> showData() {
 		return categoryService.showAllData();
 	}
-	
+
 	@DeleteMapping("/delete")
 	public String isDelete(@RequestParam int id) {
 		if (categoryService.isDeleteCategory(id)) {
@@ -47,16 +44,16 @@ public class CategoryController {
 			return "category is not deleted";
 		}
 	}
-	
+
 	@PutMapping("/update")
-	public String isUpdate(@RequestBody Category category){
+	public String isUpdate(@RequestBody Category category) {
 		if (categoryService.isUpdate(category)) {
 			return "category is updates";
+			
 		} else {
 			return "category is not updated";
 		}
-		
+
 	}
-	
-	
+
 }
